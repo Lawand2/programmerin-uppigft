d764a7bd3e6593b54e32e3a6c65ea52fada9632d
Lawand2 <71826141+Lawand2@users.noreply.github.com>
2020-11-04 15:18:49 +0100

    Add files via upload
    det är bara ett test.

---
+++ test1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text1.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal = 1;//får inte vara noll från början 
+            while (tal != 0)
+            {
+
+                Console.WriteLine("skriv din mobil nummer, 0:");
+                tal = Convert.ToInt32(Console.ReadLine());
+                if (tal % 2 == 0)
+                    Console.WriteLine("Talet är jämt");
+                else
+                    Console.WriteLine("Talet är udda");
+            }
+        }
+    }
+}
